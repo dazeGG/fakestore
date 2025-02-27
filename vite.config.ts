6df5b7dc5bd40a5e9c0ce2b011428decad53974a
VladimirBelyaev <vogisa@mail.ru>
2025-02-28 02:42:57 +0300

    tailwind: v4 -> v3.4.17

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 
 import path from 'path'
 import vue from '@vitejs/plugin-vue'
-import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,7 +11,6 @@ export default defineConfig({
 				defineModel: true,
 			},
 		}),
-		tailwindcss(),
 	],
 	resolve: {
 		alias: { '@': path.resolve(__dirname, 'src') },
