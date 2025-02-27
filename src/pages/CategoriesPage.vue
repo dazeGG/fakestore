b20087c72123fe5456cb6d9560411c562b93f3ed
VladimirBelyaev <vogisa@mail.ru>
2025-02-27 17:38:17 +0300

    init categories page as main page | removed unused files

---
+++ src/pages/CategoriesPage.vue
@@ -0,0 +1,17 @@
+<template>
+	<h1>Categories</h1>
+	{{ categories }}
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import { CategoriesServices } from '@/lib/api/services'
+
+const categories = ref<string[]>([])
+
+const loadCategories = async () => {
+	categories.value = (await CategoriesServices.getCategories()).categories
+}
+
+loadCategories()
+</script>
