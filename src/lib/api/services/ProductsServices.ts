4442bd7e5ef32fdc9c009c7184ad4fe6aa40ce9f
Vladimir <86754494+dazeGG@users.noreply.github.com>
2025-02-28 23:59:21 +0300

    feature/category-image (#8)
    * @/plugin -> @/lib/plugins
    
    * @/lib/plugin -> @/lib/plugins
    
    * added images to categories

--- src/lib/api/services/ProductsServices.ts
+++ src/lib/api/services/ProductsServices.ts
@@ -6,6 +6,19 @@ class ProductsServices {
 		return res.data
 	}
 
+	async getCategoriesImages (categories: string[]): Promise<Record<string, string>> {
+		const categoriesImages: Record<string, string> = {}
+		const requests = categories.map(category => ProductsMethods.getFirstProduct(category))
+
+		const results = await Promise.all(requests)
+
+		results.forEach((res, index) => {
+			categoriesImages[categories[index]] = res.data.products[0].image
+		})
+
+		return categoriesImages
+	}
+
 	async getProducts (category: string) {
 		const res = await ProductsMethods.getProducts(category)
 		return res.data
